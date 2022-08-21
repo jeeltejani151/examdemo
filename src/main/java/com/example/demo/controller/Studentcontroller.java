1397f74889d00a48945bf498af7d92f43bb67d73
Jeel Tejani <20it151@charusat.edu.in>
2022-08-21 21:18:16 +0530

    Added

--- src/main/java/com/example/demo/controller/Studentcontroller.java
+++ src/main/java/com/example/demo/controller/Studentcontroller.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class Studentcontroller {
     @Autowired
 
-    StudentRepository student;
+    StudentRepository studentreoository;
 
 }
